f735dd5870c67ec898b18b9bd87cc7203f824401
hasanc24 <hasan.cheema23@gmail.com>
2023-09-08 03:30:43 +0000

    updated main file

--- Lab 1/main.tf
+++ Lab 1/main.tf
@@ -61,3 +61,16 @@ tags = {
 }
 }
 
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+cidr_block     = "0.0.0.0/0"
+# gateway_id     = aws_internet_gateway.internet_gateway.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway.id
+}
+tags = {
+  Name      = "demo_private_rtb"
+  Terraform = "true"
+}
+}
+
