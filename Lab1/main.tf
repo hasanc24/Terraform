f2970ffa48f5f1a6dd6f8cf329bee0918ef0c22f
hasanc24 <hasan.cheema23@gmail.com>
2023-09-08 04:20:44 +0000

    updated

--- Lab1/main.tf
+++ Lab1/main.tf
@@ -46,15 +46,16 @@ resource "aws_subnet" "public_subnets" {
 
 #Create route tables for public and private subnets 
 resource "aws_route_table" "public_route_table" {
-vpc_id = aws_vpc.vpc.id
-route {
-  cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.internet_gateway.id
-  #nat_gateway_id = aws_nat_gateway.nat_gateway.id
-}
-tags = {
-  Name      = "demo_public_rtb"
-  Terraform = "true"
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_public_rtb"
+    Terraform = "true"
+  }
 }
 
 resource "aws_route_table" "private_route_table" {
@@ -108,5 +109,5 @@ resource "aws_nat_gateway" "nat_gateway" {
   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
   tags = {
     Name = "demo_nat_gateway"
-} 
+  }
 }
