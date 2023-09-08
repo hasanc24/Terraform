009ebc83d75c3b94787e3d4e5ededea3eb66d9fb
hasanc24 <hasan.cheema23@gmail.com>
2023-09-08 03:23:28 +0000

    updated the file

--- Lab 1/variables.tf
+++ Lab 1/variables.tf
@@ -11,3 +11,10 @@ default = "demo_vpc"
 variable "vpc_cidr" {
 type = string
 default = "10.0.0.0/16" }
+
+variable "private_subnets" { default = {
+"private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+} 
+}
