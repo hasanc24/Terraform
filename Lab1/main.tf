e5e8e6be55557fc7d868269d901872cddc8ec4ab
hasanc24 <hasan.cheema23@gmail.com>
2023-09-08 04:16:39 +0000

    updated

--- Lab1/main.tf
+++ Lab1/main.tf
@@ -44,3 +44,29 @@ resource "aws_subnet" "public_subnets" {
   }
 }
 
+#Create route tables for public and private subnets 
+resource "aws_route_table" "public_route_table" {
+vpc_id = aws_vpc.vpc.id
+route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.internet_gateway.id
+  #nat_gateway_id = aws_nat_gateway.nat_gateway.id
+}
+tags = {
+  Name      = "demo_public_rtb"
+  Terraform = "true"
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    # gateway_id     = aws_internet_gateway.internet_gateway.id
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_private_rtb"
+    Terraform = "true"
+  }
+}
+
