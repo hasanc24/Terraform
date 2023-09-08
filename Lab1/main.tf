3c05b08ea948793b6ddbed104095501e3b83280f
hasanc24 <hasan.cheema23@gmail.com>
2023-09-08 04:01:34 +0000

    updated

--- Lab1/main.tf
+++ Lab1/main.tf
@@ -97,5 +97,14 @@ resource "aws_eip" "nat_gateway_eip" {
   tags = {
     Name = "demo_igw_eip"
   }
+}
 
+#Create NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "demo_nat_gateway"
+} 
 }
