0616742a8bf211e46251bbd4e7c61a2f782c36d5
hasanc24 <hasan.cheema23@gmail.com>
2023-09-08 03:52:44 +0000

    updated

--- Lab1/main.tf
+++ Lab1/main.tf
@@ -2,3 +2,16 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+#Retrieve the list of AZs in the current AWS region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+#Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+}
