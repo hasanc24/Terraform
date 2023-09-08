7589f351d17ae202ec99e1954b030738318fdad4
hasanc24 <hasan.cheema23@gmail.com>
2023-09-08 03:24:11 +0000

    updated the file

--- Lab 1/variables.tf
+++ Lab 1/variables.tf
@@ -18,3 +18,10 @@ variable "private_subnets" { default = {
     "private_subnet_3" = 3
 } 
 }
+
+variable "public_subnets" { default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+}
+}
