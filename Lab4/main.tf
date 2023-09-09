320ffa4d92b992f2430a99652d41c870592af57e
hasanc24 <hasan.cheema23@gmail.com>
2023-09-09 22:05:01 +0000

    updated

--- Lab4/main.tf
+++ Lab4/main.tf
@@ -30,3 +30,17 @@ resource "aws_subnet" "private_subnets" {
   }
 }
 
+#Deploy the public subnets
+resource "aws_subnet" "public_subnets" {
+  for_each   = var.public_subnets
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone = tolist(data.aws_availability_zones.available.
+  names)[each.value]
+  map_public_ip_on_launch = true
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
