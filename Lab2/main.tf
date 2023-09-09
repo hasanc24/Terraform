8a2d65d1bb35fcb3a810a69a2282b8d9d79537f6
hasanc24 <hasan.cheema23@gmail.com>
2023-09-09 19:55:27 +0000

    updated

--- Lab2/main.tf
+++ Lab2/main.tf
@@ -7,3 +7,13 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 
+#Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+}
+
