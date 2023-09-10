3ae73578f13911c18d347e3b3e8eb6a6325774db
hasanc24 <hasan.cheema23@gmail.com>
2023-09-10 16:43:51 +0000

    updated

--- Lab8/main.tf
+++ Lab8/main.tf
@@ -7,6 +7,12 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 
+locals {
+  team = "api_mgmt_dev"
+application = "corp_api"
+  server_name = "ec2-${var.environment}-api-${var.variables_sub_az}"
+}
+
 #Retrieve the list of AZs in the current AWS region
 data "aws_ami" "ubuntu" {
   most_recent = true
