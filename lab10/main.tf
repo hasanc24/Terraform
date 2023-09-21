167828307c898366a10082b7201428f18ba06978
hasanc24 <hasan.cheema23@gmail.com>
2023-09-21 02:53:38 +0000

    updated

--- lab10/main.tf
+++ lab10/main.tf
@@ -11,3 +11,16 @@ provider "aws" {
   # Configuration options
 }
 
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
