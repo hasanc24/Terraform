52e37ca0c8a7ceb6c56577b9da91f0c8d046f975
hasanc24 <hasan.cheema23@gmail.com>
2023-09-21 04:33:59 +0000

    updated

---
+++ lab10/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "BlackMamba"
+
+  #  workspaces {
+  #    name = "lab10"
+  #  }
+  #}
+  
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "BlackMamba"
+
+    workspaces {
+      name = "lab10"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+locals {
+  project_name = "BlackMamba"
+}
