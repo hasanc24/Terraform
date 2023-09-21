78fb93983eafa74f08710a56e03bbc7bfc3fcffb
hasanc24 <hasan.cheema23@gmail.com>
2023-09-21 03:04:58 +0000

    updated

--- lab10/main.tf
+++ lab10/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.17.0"
     }
   }
 }
 
 provider "aws" {
-profile = "default"
+  profile = "default"
   region  = "us-east-1"
 }
 
