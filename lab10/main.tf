5faae8cd846690ad667d1af916608804685b1d55
hasanc24 <hasan.cheema23@gmail.com>
2023-09-21 04:25:57 +0000

    updated

--- lab10/main.tf
+++ lab10/main.tf
@@ -1,14 +1,3 @@
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "eu-west-1"
-  alias   = "eu"
-}
-
 variable "instance_type" {
   type = string
 }
