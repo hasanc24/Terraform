090e25f00ddf7742c7bd76410ca083a4947d33e0
hasanc24 <hasan.cheema23@gmail.com>
2023-09-21 04:56:54 +0000

    updated

--- lab10/main.tf
+++ lab10/main.tf
@@ -1,4 +1,22 @@
 terraform {
+   #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "BlackMamba"
+
+  #  workspaces {
+  #    name = "lab10"
+  #  }
+  #}
+  
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "BlackMamba"
+
+    workspaces {
+      name = "lab10"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
