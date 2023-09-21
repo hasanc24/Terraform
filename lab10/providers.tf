ba376a44a317373e48e686f50a8e972c14e16c92
hasanc24 <hasan.cheema23@gmail.com>
2023-09-21 04:26:01 +0000

    updated

--- lab10/providers.tf
+++ lab10/providers.tf
@@ -6,3 +6,14 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+  alias   = "eu"
+}
