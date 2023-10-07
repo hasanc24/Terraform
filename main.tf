b909aa9cf3eab057be5ea102364998fb852246de
hasanc24 <hasan.cheema23@gmail.com>
2023-10-07 16:46:28 +0000

    updated

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "private_subnets" {
   for_each         = var.private_subnets
   vpc_id           = aws_vpc.vpc.id
   cidr_block       = cidrsubnet(var.vpc_cidr, 8, each.value)
-  availability_zone = tolist(data.aws_availablity_zones.available.names)[each.value]
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
 
   tags = {
     Name      = each.key
@@ -35,7 +35,7 @@ resource "aws_subnet" "public_subnets" {
   for_each                = var.public_subnets
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone        = tolist(data.aws_availablity_zones.available.names)[each.value]
+  availability_zone        = tolist(data.aws_availability_zones.available.names)[each.value]
   map_public_ip_on_launch = true
   tags = {
     Name      = each.key
