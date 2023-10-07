f81226ab850c5c88b59e176c5e38f2c4e8a83752
hasanc24 <hasan.cheema23@gmail.com>
2023-10-07 16:02:04 +0000

    updated

--- main.tf
+++ main.tf
@@ -91,3 +91,12 @@ route_table_id = aws_route_table.private_route_table.id
   for_each       = aws_subnet.private_subnets
   subnet_id      = each.value.id
 }
+
+#Create Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_igw"
+  }
+}
+
