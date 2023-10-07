b1bfd93aec2eec80babb7743a0758e23a5c60512
hasanc24 <hasan.cheema23@gmail.com>
2023-10-07 16:08:11 +0000

    updated

--- main.tf
+++ main.tf
@@ -75,3 +75,17 @@ tags = {
   Terraform = "true"
 }
 }
+
+#Create route table associations
+resource "aws_route_table_association" "public" {
+  depends_on     = [aws_subnet.public_subnets]
+  route_table_id = aws_route_table.public_route_table.id
+for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
+resource "aws_route_table_association" "private" {
+  depends_on     = [aws_subnet.private_subnets]
+route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
