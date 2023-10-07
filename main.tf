a4d6483dfe86cc9a5dc2bc230d44aa1ebb23461f
hasanc24 <hasan.cheema23@gmail.com>
2023-10-07 17:30:24 +0000

    updated

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ resource "aws_vpc" "vpc" {
 
 # Deploy the Private subnets 
 resource "aws_subnet" "private_subnets" {
-  for_each         = var.private_subnets
-  vpc_id           = aws_vpc.vpc.id
-  cidr_block       = cidrsubnet(var.vpc_cidr, 8, each.value)
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
   availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
 
   tags = {
@@ -35,7 +35,7 @@ resource "aws_subnet" "public_subnets" {
   for_each                = var.public_subnets
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone        = tolist(data.aws_availability_zones.available.names)[each.value]
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
   map_public_ip_on_launch = true
   tags = {
     Name      = each.key
@@ -113,25 +113,25 @@ resource "aws_nat_gateway" "nat_gateway" {
 }
 
 # Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
-data "aws_ami" "ubuntu" { 
-most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-}
-filter {
-  name   = "virtualization-type"
-  values = ["hvm"]
-}
-owners = ["099720109477"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
 }
 
 # Terraform Resource Block - To Build EC2 instance in Public Subnet
 resource "aws_instance" "web_server" {
-ami           = data.aws_ami.ubuntu.id
-instance_type = "t2.micro"
-subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
-tags = {
-  Name = "Ubuntu EC2 Server"
-}
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "Ubuntu EC2 Server"
+  }
 }
