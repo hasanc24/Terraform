399a08af1e36ce969efec3c34347e0f886640145
hasanc24 <hasan.cheema23@gmail.com>
2023-10-07 15:32:46 +0000

    updated

--- variables.tf
+++ variables.tf
@@ -3,12 +3,3 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "vpc_name" {
-    type = string
-    default = "demo_vpc"
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
