3227bc8e0e8670b065497ca05446930dda7e5974
hasanc24 <hasan.cheema23@gmail.com>
2023-10-07 15:41:03 +0000

    updated

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ provider "aws" {
 # Retrieve the lists of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
+
+# Define the VPC 
+resource "aws_vpc" "vpc" {
+    cidr_block = var.vpc_cidr
+    tags = {
+        Name = var.vpc_name
+        Environment = "demo_environment"
+        Terraform = "true"
+    }
+}
+
