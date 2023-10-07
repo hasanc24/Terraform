3ee60eabd9c32853686e3e300d51d98b8727d445
hasanc24 <hasan.cheema23@gmail.com>
2023-10-07 16:38:40 +0000

    updated

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_route_table" "public_route_table" {
         Terraform = "true"
     }
 }
-resource "aws_route_table" "private_route_table"
+resource "aws_route_table" "private_route_table" {
 vpc_id = aws_vpc.vpc.id 
 route {
     cidr_block = "0.0.0.0/0"
