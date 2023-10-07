5f08425cc688dd1d9b1bd2380e452368387b2d06
hasanc24 <hasan.cheema23@gmail.com>
2023-10-07 15:35:06 +0000

    updated

--- variables.tf
+++ variables.tf
@@ -28,3 +28,10 @@ variable "public_subnets" {
     "public_subnet_3" = 3
   }
 }
+
+    default = {
+        "public_subnet_1" = 1
+        "public_subnet_2" = 2
+        "public_subnet_3" = 3
+    }
+}
