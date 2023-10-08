4e88316044876d30019ee2af3262849fe192e4d1
hasanc24 <hasan.cheema23@gmail.com>
2023-10-08 04:05:26 +0000

    updated

--- main.tf
+++ main.tf
@@ -151,8 +151,10 @@ resource "aws_s3_bucket" "my-new-S3-bucket" {
 tags = {
     Name = "My S3 Bucket"
     Purpose = "Intro to Resource Blocks Lab"
-} }
-resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" { bucket = aws_s3_bucket.my-new-S3-bucket.id
+} 
+}
+resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" { 
+bucket = aws_s3_bucket.my-new-S3-bucket.id
 rule {
     object_ownership = "BucketOwnerPreferred"
   }
