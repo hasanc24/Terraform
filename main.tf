a7c03324e4acf3270e58a1552eba4c14817579d7
hasanc24 <hasan.cheema23@gmail.com>
2023-10-08 04:34:29 +0000

    updated

--- main.tf
+++ main.tf
@@ -161,16 +161,16 @@ resource "aws_s3_bucket_acl" "my_new_bucket-39385938_acl" {
 resource "aws_security_group" "my-new-security-group" {
   name        = "web_server_inbound"
   description = "Allow inbound traffic on tcp/443"
-vpc_id      = aws_vpc.vpc.id
-ingress {
-  description = "Allow 443 from the Internet"
-  from_port   = 443
-to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-tags = {
-Name    = "web_server_inbound"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name    = "web_server_inbound"
     Purpose = "Intro to Resource Blocks Lab"
   }
 }
