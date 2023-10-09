ea58f7f30b9d62ec808d3d7882253e466b959060
hasanc24 <hasan.cheema23@gmail.com>
2023-10-09 03:14:24 +0000

    updated

--- main.tf
+++ main.tf
@@ -177,7 +177,9 @@ resource "aws_security_group" "my-new-security-group" {
 
 resource "aws_subnet" "variables-subnet" {
   vpc_id                  = aws_vpc.vpc.id
-cidr_block = "10.0.250.0/24" availability_zone = "us-east-1a" map_public_ip_on_launch = true
+cidr_block = "10.0.250.0/24" 
+availability_zone = "us-east-1a" 
+map_public_ip_on_launch = true
 tags = {
   Name      = "sub-variables-us-east-1a"
 Terraform = "true"
