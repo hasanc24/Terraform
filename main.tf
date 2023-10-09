53c6a99d7465a321580fff0ee43ceac58ef179a1
hasanc24 <hasan.cheema23@gmail.com>
2023-10-09 03:19:31 +0000

    updated

--- main.tf
+++ main.tf
@@ -177,7 +177,7 @@ resource "aws_security_group" "my-new-security-group" {
 
 resource "aws_subnet" "variables-subnet" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.250.0/24"
+  cidr_block              = "var.variables_sub_cidr
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
