621a336d35c3efc7c8e3ade2c87a6a8bdb17bd2b
hasanc24 <hasan.cheema23@gmail.com>
2023-10-09 03:20:49 +0000

    updated

--- main.tf
+++ main.tf
@@ -179,7 +179,7 @@ resource "aws_subnet" "variables-subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.variables_sub_cidr
   availability_zone       = var.varitables_sub_az
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = var.variables_sub_auto_ip
   tags = {
     Name      = "sub-variables-us-east-1a"
     Terraform = "true"
