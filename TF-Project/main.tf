3de4e2b0de43b0d1739da8a1c4805141cd2e30ec
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 00:26:51 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my-first-server" {
-ami = "ami-0dbc3d7bc646e8516"
-instance_type = "t2.micro"    
+  ami           = "ami-0dbc3d7bc646e8516"
+  instance_type = "t2.micro"
 }
 
 
