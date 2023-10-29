30007e6cba1736cb37a8762eca29d99de6f18712
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 17:58:35 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -2,19 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "My-Frist-VPC" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "Production"
-  }
-}
-
 resource "aws_subnet" "subnet-1" {
   vpc_id     = aws_vpc.My-Frist-VPC.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "Prod-subnet"
+  }
+}
+
+resource "aws_vpc" "My-Frist-VPC" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Production"
   }
 }
 
