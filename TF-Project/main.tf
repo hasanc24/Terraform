5153eedf3c360902fb077bc54c0709c6e1f2ff2c
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 18:48:55 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -13,13 +13,13 @@ provider "aws" {
 }
 
 #Create a VPC 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
 #Create Internet Gateway
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.prod-vpc.id
 
   tags = {
     Name = "main"
