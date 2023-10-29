ca59a3474b11d7e80b5d4235db8009f2c42d9e38
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 19:33:04 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -119,3 +119,11 @@ resource "aws_network_interface" "web-server-nic" {
   security_groups = [aws_security_group.allow_web.id]
 }
 
+#Assign an Elastic IP to the Network Interface created in Step #7
+region "aws_eip" "one" {
+  vpc = true
+  network_interface = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = aws_internet_gateway.gw
+}
+
