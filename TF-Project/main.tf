02d9f36af7cebd2ba563fd2fadd43cf4ecfb31dc
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 20:48:54 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -153,3 +153,8 @@ resource "aws_instance" "web-server-instance" {
     Name = "web-server"
   }
 }
+
+output "server_private_ip" {
+  value = aws_instance.web-server-instance.private_ip
+  value = aws_instance.web-server-instance.id
+}
