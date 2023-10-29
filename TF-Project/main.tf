d685c5ea64fe147e39994899f250290c328016e9
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 20:00:18 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -10,9 +10,8 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
 }
+
 #Create a VPC 
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
