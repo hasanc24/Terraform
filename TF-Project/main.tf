158e6ddfbb535a99aef8d4b6b8fcab0a91328010
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 19:19:31 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -82,18 +82,18 @@ resource "aws_security_group" "allow_web" {
   }
 
 ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
   }
   
   ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
+    description      = "SSH"
+    from_port        = 2
+    to_port          = 2
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
