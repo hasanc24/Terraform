2422d2e61f6f54a5b493c09947f676a5500c4102
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 18:52:11 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -9,9 +9,10 @@
 #9 Create Ubuntu server and install/enable apache2
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
-
 #Create a VPC 
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
