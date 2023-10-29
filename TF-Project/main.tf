5e111241dd2e8c15e86b2398e9d3b44c4c278624
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 17:24:30 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 resource "aws_instance" "My-First-Server" {
   ami           = "ami-0dbc3d7bc646e8516"
   instance_type = "t2.micro"
@@ -5,6 +10,8 @@ resource "aws_instance" "My-First-Server" {
     name = "My-First-Sever"
   }
 }
+
+
 
 
 
