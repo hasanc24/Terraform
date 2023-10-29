47afc76db18d456be131330971a73783407959b0
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 17:46:09 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "My-Frist-VPC" {
 }
 
 resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.My-Frist-VPC.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
