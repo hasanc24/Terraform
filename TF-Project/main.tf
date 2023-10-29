796baa6df6172cc7e3ebe2ce026d2b04eed5a9d8
hasanc24 <hasan.cheema23@gmail.com>
2023-10-29 17:25:32 +0000

    updated

--- TF-Project/main.tf
+++ TF-Project/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "My-First-Server" {
-  ami           = "ami-0dbc3d7bc646e8516"
-  instance_type = "t2.micro"
-  tags = {
-    name = "My-First-Sever"
-  }
-}
+#resource "aws_instance" "My-First-Server" {
+#  ami           = "ami-0dbc3d7bc646e8516"
+#  instance_type = "t2.micro"
+#  tags = {
+#    name = "My-First-Sever"
+#  }
+#}
 
 
 
