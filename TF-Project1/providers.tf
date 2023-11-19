67278490ce67f74b0d20c59390a61a705969ef7c
hasanc24 <hasan.cheema23@gmail.com>
2023-11-19 02:01:12 +0000

    updated

--- TF-Project1/providers.tf
+++ TF-Project1/providers.tf
@@ -8,7 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  shared_config_files      = ["/Users/tf_user/.aws/conf"]
-  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
-  profile                  = "customprofile"
-}
+  region = "us-east-1"
+  shared_credentials_file = "/Users/tf_user/.aws/creds"
+  profile = :customprofile" 
+  }
+  
+  
