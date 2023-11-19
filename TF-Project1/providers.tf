a9f9597daacb63f046c56d2b63cbd25d943289ad
hasanc24 <hasan.cheema23@gmail.com>
2023-11-19 02:54:25 +0000

    updated

--- TF-Project1/providers.tf
+++ TF-Project1/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region                  = "us-east-1"
-  shared_credential_file = "~/.aws/credentials"
+  shared_credentials_file = "~/.aws/credentials"
   profile                 = "default"
 }
 
