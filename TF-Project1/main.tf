53bc9a21abfe54f740353df66a0976de0372cbd7
hasanc24 <hasan.cheema23@gmail.com>
2023-11-19 02:21:11 +0000

    updated

--- TF-Project1/main.tf
+++ TF-Project1/main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostname = true 
-  enable_dns_support = true 
-  
+  cidr_block          = "10.0.0.0/16"
+  enable_dns_hostname = true
+  enable_dns_support  = true
+
   tags = {
-      Name = "Dev"
+    Name = "Dev"
   }
 }
