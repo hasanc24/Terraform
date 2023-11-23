0529a500fb0718521163c9cca363df36d39ffb68
hasanc24 <hasan.cheema23@gmail.com>
2023-11-23 04:52:33 +0000

    updated

--- TF-Project1/main.tf
+++ TF-Project1/main.tf
@@ -159,16 +159,18 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_s3_bucket" "my-new-S3-bucket" {   
+  bucket = "my-new-tf-test-bucket-bryan"
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    gateway_id     = aws_internet_gateway.internet_gateway.id
-    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-  tags = {
-    Name      = "demo_public_rtb"
-    Terraform = "true"
+  tags = {     
+    Name = "My S3 Bucket"     
+    Purpose = "Intro to Resource Blocks Lab"   
+  } 
+}
+
+resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" {   
+  bucket = aws_s3_bucket.my-new-S3-bucket.id  
+  rule {     
+    object_ownership = "BucketOwnerPreferred"   
   }
 }
