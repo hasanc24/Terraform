8929e2344d00ffb6dab22cdcfa26579c80a8a8ae
hasanc24 <hasan.cheema23@gmail.com>
2023-11-23 05:06:33 +0000

    updated

--- TF-Project1/terraform.tf
+++ TF-Project1/terraform.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
+      source = "hashicorp/aws"
+      version = "5.26.0"
     }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
