00ca77c7673a4dff7937486a610a6e242886c308
hasanc24 <hasan.cheema23@gmail.com>
2023-11-23 04:56:12 +0000

    updated

--- TF-Project1/main.tf
+++ TF-Project1/main.tf
@@ -159,16 +159,16 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_s3_bucket" "my-new-S3-bucket" {   
+resource "aws_s3_bucket" "my-new-S3-bucket" {
   bucket = "my-new-tf-test-bucket-hasanc"
 
-  tags = {     
-    Name = "My S3 Bucket"     
-    Purpose = "Intro to Resource Blocks Lab"   
-  } 
+  tags = {
+    Name    = "My S3 Bucket"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
 }
 
-resource "aws_s3_bucket_acl" "my_new_bucket_acl" {   
-  bucket = aws_s3_bucket.my-new-S3-bucket.id  
-  acl = "private"
+resource "aws_s3_bucket_acl" "my_new_bucket_acl" {
+  bucket = aws_s3_bucket.my-new-S3-bucket.id
+  acl    = "private"
 }
